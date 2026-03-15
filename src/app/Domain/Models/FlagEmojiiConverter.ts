d7528c34464296bb716a2a6da4c6e41f61ae83b2
agavrilov <agavrilov@quirco.local>
2026-03-15 17:02:40 +0200

    Add new currencies icons

--- src/app/Domain/Models/FlagEmojiiConverter.ts
+++ src/app/Domain/Models/FlagEmojiiConverter.ts
@@ -5,6 +5,8 @@ export abstract class FlagEmojiiConverter
     '🇮🇱': 'https://flagcdn.com/w20/il.png',
     '🇺🇸': 'https://flagcdn.com/w20/us.png',
     '🇪🇺': 'https://flagcdn.com/w20/eu.png',
+    '🇨🇳': 'https://flagcdn.com/w20/cn.png',
+    '🇯🇴': 'https://flagcdn.com/w20/jo.png',
   }
 
   static getSrcByEmojiCode(code: string): string {
